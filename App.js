33e9e02eb668caa46153d6206a3a1f74acd42eb1
Andrew <90443266+Andrew2865@users.noreply.github.com>
2023-06-23 19:05:30 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React,{useState,useEffect} from 'react';
 import MapView,{Marker} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import * as Location from 'expo-location';
+
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-
+ 
+  
   useEffect(() => {
     (async () => {
       
@@ -27,6 +29,7 @@ export default function App() {
     text = JSON.stringify(location);
   }
   
+  
   return (
     <View style={styles.container}>
      
@@ -39,6 +42,7 @@ export default function App() {
       latitudeDelta: 0.015,
       longitudeDelta: 0.0121,
     }}
+    
       
       >
        <Marker
@@ -46,6 +50,7 @@ export default function App() {
       latitude:49.7803014086921, 
       longitude: 22.76600752434019 ,
     }}
+    title={"Miś Generał"}
     />
 
 <Marker
@@ -53,6 +58,7 @@ export default function App() {
       latitude:49.78591891571912, 
       longitude: 22.76611810224204,
     }}
+    title={"Miś hydraulik"}
     />
     
 <Marker
@@ -60,6 +66,7 @@ export default function App() {
       latitude:49.78370479629267, 
       longitude: 22.76692201994785,
     }}
+    title={"Miś informatyk"}
 />
 
 <Marker
@@ -67,18 +74,21 @@ export default function App() {
       latitude:49.78306627681199, 
       longitude: 22.77682148980239,
     }}
+    title={"Miś kierowca autobusu"}
 />
 <Marker
     coordinate={{
       latitude:49.78171054713026, 
       longitude: 22.77135283939332,
     }}
+    title={"Miś mechanik"}
 />
-<Marker 
+<Marker
 coordinate={{
 latitude:49.78209690716262, 
 longitude:22.762122937536446,
 }}
+title={"Miś z choinką"}
 />
 
 <Marker 
@@ -86,6 +96,7 @@ coordinate={{
 latitude:49.78182748533911, 
 longitude:22.77340301544233 ,
 }}
+title={"Miś z dzwonem"}
 />
 
 <Marker 
@@ -93,13 +104,15 @@ coordinate={{
 latitude:49.783096274788896,
 longitude: 22.771407289376196,
 }}
+title={"Miś z fajką"}
 />
 
-<Marker 
+<Marker
 coordinate={{
 latitude:49.78243512184017, 
 longitude: 22.771968217292873,
 }}
+title={"Miś z lodami"}
 />
 
 <Marker 
@@ -107,13 +120,15 @@ coordinate={{
 latitude:49.783900434378204,
 longitude: 22.77194183091356,
 }}
+title={"Miś z okularem"}
 />
 
-<Marker 
+<Marker
 coordinate={{
 latitude:49.78243022558096, 
 longitude: 22.771306713491533,
 }}
+title={"Miś z perfumami"}
 />
 
 <Marker 
@@ -121,6 +136,7 @@ coordinate={{
 latitude:49.78306141028644, 
 longitude: 22.76808612902565 ,
 }}
+title={"Miś z pizzą"}
 />
 
 
